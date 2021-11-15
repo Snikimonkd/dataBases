40de89c1c0894ce4b32d7e8ee7243fec0fe2580c
Snikimonkd <cepminkis132@gmail.com>
2021-11-15 15:40:15 +0300

    пофиксить хиты

--- internal/utils/metrics/prometheus.go
+++ internal/utils/metrics/prometheus.go
@@ -2,8 +2,6 @@ package metrics
 
 import (
 	"net/http"
-	"strconv"
-	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -15,11 +13,11 @@ var (
 func New() {
 	hits = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "hits",
-	}, []string{"node1"})
+	}, []string{"node_name"})
 
 	prometheus.MustRegister(hits)
 }
 
 func CreateRequestHits(status int, r *http.Request) {
-	hits.WithLabelValues(strconv.Itoa(status), strings.Split(r.URL.Path, "/")[2], r.Method).Inc()
+	hits.WithLabelValues("node_1").Inc()
 }
